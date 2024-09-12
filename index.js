1e0a0ee7dbb1782d8f4d0bd3c2b800604acea6bd
rla1wo23@gmail.com <rla1wo23@gmail.com>
2024-09-12 18:47:48 +0900

    fix: files

--- index.js
+++ index.js
@@ -1,97 +1,156 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+const redis = require("redis");
+const mysql = require("mysql2/promise");
+require("dotenv").config();
+
 const app = express();
 const port = 3000;
-const mysql = require('mysql2/promise');
 
+// MySQL 연결 풀 설정
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
-//RDS MySQL 데이터베이스 연결 설정
+
 console.log("DB Host:", process.env.DB_HOST);
 console.log("DB User:", process.env.DB_USER);
 console.log("DB Password:", process.env.DB_PASSWORD);
 console.log("DB Name:", process.env.DB_NAME);
 
-app.get('/', function(req, res) {
-  res.send('Hello World!');
+// Redis 클라이언트 생성
+const redisClient = redis.createClient({
+  url: process.env.REDIS_URL,
 });
-
-// CORS 미들웨어 사용
+redisClient.connect().catch(console.error);
 app.use(cors());
-
-// JSON 요청 본문을 파싱하기 위한 미들웨어
 app.use(express.json());
 
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+// 기본 경로, LB가 체크하는 경로
+app.get("/", function (req, res) {
+  res.send("Hello Load Balancer!");
 });
 
+// 서버 실행
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
 
 // 영화 목록 API
-app.get('/api/movies', async (req, res) => {
-    try {
-        console.log("DB Name:", process.env.DB_NAME);
-        const [rows] = await pool.query('SELECT * FROM Movies');
-        res.json(rows);
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Server error from getting Movie');
-    }
+app.get("/api/movies", async (req, res) => {
+  try {
+    const [rows] = await pool.query("SELECT * FROM Movies");
+    res.json(rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server error from getting movies");
+  }
 });
 
 // 특정 영화의 상영회차 정보 API
-app.get('/api/screens/:movieId', async (req, res) => {
-    const { movieId } = req.params;
-    try {
-        const [rows] = await pool.query('SELECT * FROM Screens WHERE MovieID = ?', [movieId]);
-        res.json(rows);
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Server error from getting screens for MovieID');
-    }
+app.get("/api/screens/:movieId", async (req, res) => {
+  const { movieId } = req.params;
+  try {
+    const [rows] = await pool.query("SELECT * FROM Screens WHERE MovieID = ?", [
+      movieId,
+    ]);
+    res.json(rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server error from getting screens for MovieID");
+  }
 });
-
 
 // 특정 세션의 좌석 정보 API
-app.get('/api/seats/:screenId', async (req, res) => {
-    const { screenId } = req.params;
-    try {
-        const [rows] = await pool.query('SELECT * FROM Seats WHERE ScreenID = ?', [screenId]);
-        res.json(rows);
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Server error from getting seats for ScreenID');
+app.get("/api/seats/:screenId", async (req, res) => {
+  const { screenId } = req.params;
+  try {
+    // Redis에서 좌석 정보 조회
+    let seats = await redisClient.get(`seats:${screenId}`);
+
+    if (seats) {
+      // Redis에서 좌석 정보가 있는 경우
+      console.log(`Seats for screen ${screenId} found in Redis.`);
+      return res.status(200).json(JSON.parse(seats));
+    } else {
+      // Redis에 좌석 정보가 없는 경우
+      console.log(
+        `Seats for screen ${screenId} not found in Redis. Fetching from RDS...`,
+      );
+      const [rows] = await pool.query(
+        "SELECT SeatID, Status FROM Seats WHERE ScreenID = ?",
+        [screenId],
+      );
+
+      if (rows.length === 0) {
+        return res
+          .status(404)
+          .json({ error: "No seats found for this screen." });
+      }
+
+      // 좌석정보 캐싱
+      await redisClient.set(`seats:${screenId}`, JSON.stringify(rows), {
+        EX: 60 * 5, // 5분 TTL
+      });
+
+      console.log(`Seats for screen ${screenId} cached in Redis.`);
+      return res.status(200).json(rows);
     }
+  } catch (error) {
+    console.error("Error fetching seat information:", error);
+    res.status(500).send("Server error");
+  }
 });
 
+// 좌석 예약 API
+app.post("/api/seats/reserve", async (req, res) => {
+  const { screenId, seatId } = req.body; // 요청 본문에서 screenId와 seatId 값을 가져옴
+  try {
+    //REDIS 좌석 조회
+    let seatStatus = await redisClient.get(`seat:${screenId}:${seatId}`);
 
-// 좌석 예약
-app.post('/api/seats/reserve', async (req, res) => {
-    const { screenId, seatsId } = req.body; // 요청 본문에서 screenId, seatsId 값을 가져옵니다.
-    try {
-        const updateQuery = `
-            UPDATE Seats
-            SET Reservations = true
-            WHERE ScreenID = ? AND SeatsID = ?;
-        `;
-        // DB 쿼리 실행
-        const [result] = await pool.query(updateQuery, [screenId, seatsId]);
+    if (seatStatus === null) {
+      // Redis에 좌석 정보가 없으면 RDS에서 조회
+      console.log(
+        `Seat ${seatId} for screen ${screenId} not found in Redis. Fetching from RDS...`,
+      );
+      const [rows] = await pool.query(
+        "SELECT Status FROM Seats WHERE ScreenID = ? AND SeatID = ?",
+        [screenId, seatId],
+      );
 
-        if (result.affectedRows > 0) {
-            res.send({ message: 'Seat reservation updated successfully.' });
-        } else {
-            res.status(404).send({ message: 'Seat not found or already reserved.' });
-        }
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Server error from seat reservation');
+      if (rows.length === 0) {
+        return res.status(404).json({ error: "Seat not found" });
+      }
+      seatStatus = rows[0].Status;
+
+      // 좌석 캐싱
+      await redisClient.set(`seat:${screenId}:${seatId}`, seatStatus, {
+        EX: 60 * 5, // 5분 TTL
+      });
+
+      console.log(`Seat ${seatId} for screen ${screenId} cached in Redis.`);
     }
-});
+    // 좌석 예약 여부 확인
+    if (seatStatus === "reserved") {
+      return res.status(400).json({ error: "Seat already reserved" });
+    }
 
+    // 동기화
+    await redisClient.set(`seat:${screenId}:${seatId}`, "reserved"); // Redis
+    await pool.query(
+      'UPDATE Seats SET Status = "reserved" WHERE ScreenID = ? AND SeatID = ?',
+      [screenId, seatId],
+    );
+    //RDS
+    res.status(200).json({ message: "Seat reserved successfully" });
+  } catch (error) {
+    console.error("Error reserving seat:", error);
+    res.status(500).json({ error: "Server error" });
+  }
+});
